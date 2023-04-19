f7c99610b59f99facc442ac431a7ff42b0b9871e
Rajat Gupta <rajatinvoice@gmail.com>
2023-04-19 17:04:37 +0530

    First Upload

---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,30 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { SearchFormComponent } from '../components/search-form/search-form.component';
+import { MatSelectModule } from '@angular/material/select';
+import { MatInputModule } from '@angular/material/input';
+import { MatIconModule } from '@angular/material/icon';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatNativeDateModule } from '@angular/material/core';
+import { NgxMaterialTimepickerModule, NgxMaterialTimepickerToggleIconDirective  } from 'ngx-material-timepicker';
+
+
+
+@NgModule({
+  declarations: [
+    SearchFormComponent
+  ],
+  imports: [
+    CommonModule,
+    MatSelectModule,
+    MatInputModule,
+    MatIconModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
+    NgxMaterialTimepickerModule 
+  ],
+  exports: [
+    SearchFormComponent
+  ]
+})
+export class SharedModule { }
